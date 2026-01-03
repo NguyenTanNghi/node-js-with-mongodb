45b9474d6366b82ae92eaa957f0a1c38ad558e46
NguyenTanNghi <nguyentannghi5722@gmail.com>
2026-01-03 13:23:32 +0700

    di chuyển code server vào thư mục src và cập nhật script trong package.json

--- src/server.js
+++ src/server.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const path = require("path");
 // import express from "express";
-const app = express();
-const port = 8081;
 
+const app = express();
+const port = 8080;
+
+// config view engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// config routes
 app.get("/", (req, res) => {
     res.send("Hello World!");
+});
+app.get("/sample", (req, res) => {
+    res.render("sample");
 });
 
 app.listen(port, () => {
