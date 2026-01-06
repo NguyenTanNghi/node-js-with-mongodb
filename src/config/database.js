da112c2f1d753022f274aa297964ec648763d093
NguyenTanNghi <nguyentannghi5722@gmail.com>
2026-01-06 13:19:23 +0700

    Cập nhật thông báo kết nối cơ sở dữ liệu và cải thiện cấu trúc mô hình Kitten

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,10 @@ const { default: mongoose } = require("mongoose");
 require("dotenv").config();
 
 const dbState = [
-    { value: 0, label: "disconnected" },
-    { value: 1, label: "connected" },
-    { value: 2, label: "connecting" },
-    { value: 3, label: "disconnecting" },
+    { value: 0, label: "Disconnected" },
+    { value: 1, label: "Connected" },
+    { value: 2, label: "Connecting" },
+    { value: 3, label: "Disconnecting" },
 ];
 
 const connection = async () => {
@@ -16,6 +16,6 @@ const connection = async () => {
     };
     await mongoose.connect(process.env.DB_HOST, options);
     const state = Number(mongoose.connection.readyState);
-    console.log(dbState.find((f) => f.value === state).label, "to db");
+    console.log(dbState.find((f) => f.value === state).label, "to database");
 };
 module.exports = connection;
--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require("express"); //commonjs
 const configViewEngine = require("./config/viewEngine");
 const webRouter = require("./routers/web");
 const connection = require("./config/database");
-const mongoose = require("mongoose");
+const Kitten = require("./models/Kitten");
 
 const app = express();
 const port = process.env.PORT || 8081;
@@ -19,14 +19,10 @@ configViewEngine(app);
 // config routes
 app.use("/", webRouter); // tất cả đường link trong webRouter đều bắt đầu bằng "/"
 
-const kittySchema = new mongoose.Schema({
-    name: String,
-});
-const Kitten = mongoose.model("Kitten", kittySchema);
-const cat = new Kitten({ name: "Hoi Dan IT Cat TEST" });
+// tạo mô hình và lưu document vào collection
+const cat = new Kitten({ name: "Hoi Dan IT Cat1232" });
 cat.save();
 
-// test connect db
 // kết nối db và sau khi kết nối thành công thì mới chạy server
 (async () => {
     try {
@@ -40,3 +36,4 @@ cat.save();
         console.log(">>> Error connect to db: ", error);
     }
 })();
+ 
