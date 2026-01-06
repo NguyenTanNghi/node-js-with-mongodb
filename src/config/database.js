56852b1122a715ea1c5fd50d43c475b86e49834d
NguyenTanNghi <nguyentannghi5722@gmail.com>
2026-01-06 11:02:25 +0700

    thiết lập kết nối database

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,18 @@
+const { default: mongoose } = require("mongoose");
 require("dotenv").config();
-const mongoose = require("mongoose");
+
+const dbState = [
+    { value: 0, label: "disconnected" },
+    { value: 1, label: "connected" },
+    { value: 2, label: "connecting" },
+    { value: 3, label: "disconnecting" },
+];
 
 const connection = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect("mongodb://root:123456@localhost:27018");
+        const state = Number(mongoose.connection.readyState);
+        console.log(dbState.find((f) => f.value === state).label, "to db"); 
         console.log("Connected to MongoDB successfully");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
